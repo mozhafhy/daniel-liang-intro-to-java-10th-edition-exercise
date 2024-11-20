4c8b2b29e80fc34b5af174fd0520d83ec8de161b
kaila asus <mochzhafif1123@student.ub.ac.id>
2024-11-20 07:39:31 +0700

    Initial commit

--- exercise-05/lat5_22_$$.java
+++ exercise-05/lat5_22_$$.java
@@ -1,4 +1,14 @@
 /*
+ * Loan Amount: 10000
+ * Number of Years: 1
+ * Annual Interest Rate: 7
+ * 
+ * Monthly Payment: 865.26
+ * Total Payment: 10383.21
+ * 
+ * tabelnya di bawah sini
+ * Payment#		Interest		Principal		Balance
+ * ...				...					...					...
  * 
  * Rumus:
  * monthlyPayment = (loanAmount * monthlyInterestRate) / 1 - Math.pow(1 + monthlyInterestRate, -numOfYears * 12)
@@ -6,8 +16,40 @@
  * 
  */
 
+import java.util.Scanner;
+
 public class lat5_22_$$ {
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Loan Amount: ");
+		double loanAmount = in.nextDouble();
+
+		System.out.print("Number of Years: ");
+		int numberOfYears = in.nextInt();
+
+		System.out.print("Annual Interest Rate: ");
+		double annualInterestRate = in.nextDouble();
+		double monthlyInterestRate = annualInterestRate / 1200.0;
+
+		double monthlyPayment = (loanAmount * monthlyInterestRate) / 1
+				- Math.pow(1 + monthlyInterestRate, -numberOfYears * 12);
+		double totalPayment = monthlyPayment * numberOfYears * 12;
+		System.out.printf("\nMonthly Payment: %.2f\n", monthlyPayment);
+		System.out.printf("Total Payment: %.2f\n\n", totalPayment);
+
+		double balance = loanAmount;
+		double interest = monthlyInterestRate * balance;
+		double principal;
+
+		System.out.println("Payment#\t\tInterest\t\tPrincipal\t\tBalance");
+		for (int i = 1; i <= numberOfYears * 12; i++) {
+			interest = monthlyInterestRate * balance;
+			principal = monthlyPayment - interest;
+			balance = balance - principal;
+			System.out.println(i + "\t\t" + interest + "\t\t" + principal + "\t\t" + balance);
+		}
+
+		in.close();
 	}
 }
